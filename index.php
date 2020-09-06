a2e925ad4ff3d2fb6ed5ede9055f46be01aa8b0a
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-09-06 18:35:50 +0800

    Update v0.1.a

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
     </head>
     <body class="body">
         <div class="container shadow-sm p-3 mb-5 bg-white rounded">
+<!--
             <div class="header">
+                
+            </div>
+-->
+            <div class="content">
                 <div class="row">
                     <div class="col-sm-2">
                         <img src="assets/img/logo/Logo_GPIB.png" style="width:80%">
@@ -24,7 +29,7 @@
                     </div>
                 </div>
                 <br>
-                <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top navbar-inverse rounded">
+                <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top rounded">
                     <ul class="nav navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Beranda</a>
@@ -56,8 +61,6 @@
                     </ul>
                 </nav>
                 <hr>
-            </div>
-            <div class="content">
                 <div class="row">
                     <div class="col">
                         <div class="card bg-primary text-white">
@@ -68,7 +71,7 @@
                                 </center>
                                 <p class="card-text">
                                     <center>
-                                        Sesuai dengan instruksi Pemerintah Republik Indonesia dan Surat Majelis Sinode GPIB untuk memutus mata rantai penyebaran Covid-19 (Virus Corona), maka Majelis Jemaat GPIB "Kasih Karunia" Denpasar - Bali Mengalihkan Kegiatan Ibadah Minggu dan Seluruh Ibadah Pelkat di rumah masing-masing sampai batas waktu yang akan ditentukan atau pengumuman selanjutnya. Mari kita laksanakan dengan penuh tanggung jawab. Tuhan Yesus Memberkati kita semua.
+                                        Sesuai dengan instruksi Pemerintah Republik Indonesia dan Surat Majelis Sinode GPIB untuk memutus mata rantai penyebaran Covid-19 (Virus Corona), maka Majelis Jemaat GPIB "Kasih Karunia" Denpasar - Bali Mengalihkan Kegiatan Ibadah Minggu dan Seluruh Ibadah Pelkat di rumah masing-masing sampai batas waktu yang akan ditentukan atau pengumuman selanjutnya. Ibadah akan dilakukan secara online melalui channel Youtube <a href="https://bit.ly/GPIBKaKaDen" style="color:white;"><strong>GPIB KaKaDen.</strong></a> Mari kita laksanakan dengan penuh tanggung jawab. Tuhan Yesus Memberkati kita semua.
                                     </center>
                                 </p>
                             </div>
@@ -76,7 +79,6 @@
                     </div>
                 </div>
                 <br>
-                <div class="content-header">
                     <div class="row">
                         <div class="col">
                             <div class="embed-responsive embed-responsive-16by9 rounded">
@@ -85,23 +87,190 @@
                             <hr>
                         </div>
                     </div>
-                </div>
                 <div class="row">
                     <div class="col-sm-8">
                     isi
                     </div>
                     <div class="col-sm-4">
-                    sidebar
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Download</h4>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <a href="https://bit.ly/TataIbadahKKDN"><i><img src="assets/img/ico/download.svg"></i> Tata Ibadah Minggu</a><br>
+                                    <a href="https://bit.ly/WartaJemaatKKDN"><i><img src="assets/img/ico/download.svg"></i> Warta Jemaat</a>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Berita</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="obyekBerita">
+                                    <a href="#">
+                                        <h5 class="card-title" style="">Berita Duka - 05-07-2020</h5>
+                                        <p class="card-text">
+                                            Telah dimuliakan oleh Bapa di Surga...
+                                        </p>
+                                    </a>
+                                    <hr>
+                                </div>
+                                <div class="obyekBerita">
+                                    <a href="#">
+                                        <h5 class="card-title">Mamang</h5>
+                                        <p class="card-text">
+                                            fdsafdsa
+                                        </p>
+                                    </a>
+                                    <hr>
+                                </div>
+                                <div class="obyekBerita">
+                                    <a href="#">
+                                        <h5 class="card-title">IHMPA</h5>
+                                        <p class="card-text">
+                                            07-07-077
+                                        </p>
+                                    </a>
+                                    <hr>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Jadwal Ibadah Minggu</h4>
+                            </div>
+                            <div class="card-body">
+                                <ul>
+                                    <li>Pukul 07.00 WITA</li>
+                                    <li>Pukul 10.00 WITA</li>
+                                    <li>Pukul 18.00 WITA</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Jadwal Ibadah Pelkat</h4>
+                                <h6>(Pelayanan Kategorial) rencananya banner</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="card pakakaden">
+                                    <div class="card-header">
+                                        <h4>Pelayanan Anak</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/PA.png" style="width:40%;">
+                                            <div class="p-1">
+                                                <br>
+                                                Setiap Pukul 07.00 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card ptkakaden">
+                                    <div class="card-header" style="color:black;">
+                                        <h4>Persekutuan Teruna</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/PT.jpg" style="width:40%;">
+                                            <div class="p-1" style="color:black;">
+                                                <br>
+                                                Setiap Pukul 07.00 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card gpkakaden">
+                                    <div class="card-header">
+                                        <h4>Gerakan Pemuda</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/GP.png" style="width:40%;">
+                                            <div class="p-1">
+                                                <br>
+                                                Setiap Pukul 19.30 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card pkpkakaden">
+                                    <div class="card-header">
+                                        <h4>Persekutuan Kaum Perempuan</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/PKP.png" style="width:40%;">
+                                            <div class="p-1">
+                                                <br>
+                                                Setiap Pukul 17.30 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card pkbkakaden">
+                                    <div class="card-header">
+                                        <h4>Persekutuan Kaum Bapak</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/PKB.png" style="width:40%;">
+                                            <div class="p-1">
+                                                <br>
+                                                Setiap Pukul 19.30 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="card pklukakaden">
+                                    <div class="card-header">
+                                        <h4>Persekutuan Kaum Lanjut Usia</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="d-flex p-2 flex-row">
+                                            <img src="assets/img/logo/PKLU-putih.png" style="width:40%;">
+                                            <div class="p-1">
+                                                <br>
+                                                Setiap Pukul 19.30 WITA
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="footer rounded">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <p class="footerp">
+                            mamank
+                        </p>
+                    </div>
+                    <div class="col-sm-4">
+                        <p class="footerp">
+                            mamank
+                        </p>
+                    </div>
+                    <div class="col-sm-4">
+                        <p class="footerp">
+                            mamank
+                        </p>
                     </div>
                 </div>
-                
-                
-                
-                
-                
-            </div>
-            <div class="footer">
-            
             </div>
         </div>
     </body>
