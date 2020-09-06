6595ac688f2e96272fcecd46348086bbab8963bc
aldobagus725 <50010046+aldobagus725@users.noreply.github.com>
2020-09-06 12:39:27 +0800

    GPIB KaKaDen v0.1a Structure
    Alpha 1

---
+++ index.php
@@ -0,0 +1,110 @@
+<html>
+    <head>
+        <title>GPIB KaKaDen</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/kakaden.css">
+        <link rel="shortcut icon" href="assets/img/logo/Logo_GPIB%20-YoutubeBadge.png" type="image/x-icon"/>
+    </head>
+    <body class="body">
+        <div class="container shadow-sm p-3 mb-5 bg-white rounded">
+            <div class="header">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img src="assets/img/logo/Logo_GPIB.png" style="width:80%">
+                    </div>
+                    <div class="col-sm-8">
+                        <h2>Gereja Protestan di Indonesia bagian Barat</h2>
+                        <h3>Jemaat "Kasih Karunia" Denpasar</h3>
+                        <p class="card-text">
+                            Alamat : BTN Dalung Permai Blok D/45
+                        </p>
+                    </div>
+                </div>
+                <br>
+                <nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top navbar-inverse rounded">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sakramen</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hari Raya Gerejawi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Aktifitas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Renungan</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right ml-auto">
+                        <li>
+                            <form class="form-inline" action="/action_page.php">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">Search</span>
+                                    </div>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+                <hr>
+            </div>
+            <div class="content">
+                <div class="row">
+                    <div class="col">
+                        <div class="card bg-primary text-white">
+                            <div class="card-body">
+                                <center>
+                                    <h2 class="card-title">Perhatian!</h2>
+                                    <h4 class="card-title">Pemberitahuan tentang Kegiatan dan Ibadah terkait dengan COVID-19!</h4>
+                                </center>
+                                <p class="card-text">
+                                    <center>
+                                        Sesuai dengan instruksi Pemerintah Republik Indonesia dan Surat Majelis Sinode GPIB untuk memutus mata rantai penyebaran Covid-19 (Virus Corona), maka Majelis Jemaat GPIB "Kasih Karunia" Denpasar - Bali Mengalihkan Kegiatan Ibadah Minggu dan Seluruh Ibadah Pelkat di rumah masing-masing sampai batas waktu yang akan ditentukan atau pengumuman selanjutnya. Mari kita laksanakan dengan penuh tanggung jawab. Tuhan Yesus Memberkati kita semua.
+                                    </center>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="content-header">
+                    <div class="row">
+                        <div class="col">
+                            <div class="embed-responsive embed-responsive-16by9 rounded">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/?listType=playlist&list=PLvuzSH6_9oLoG2K7ie9I7rtNJijhQ5wGc" allowfullscreen></iframe>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8">
+                    isi
+                    </div>
+                    <div class="col-sm-4">
+                    sidebar
+                    </div>
+                </div>
+                
+                
+                
+                
+                
+            </div>
+            <div class="footer">
+            
+            </div>
+        </div>
+    </body>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</html>
